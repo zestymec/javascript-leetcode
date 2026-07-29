a08835c62de81dce84455575410b3f66f39560ee
M Umer Aziz <m.umer394aziz@gmail.com>
2026-07-29 23:43:21 +0500

    Time: 40 ms (75.58%), Space: 53.2 MB (75.09%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,9 @@
+var map = function(arr, fn) {
+    const returnedArray = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        returnedArray.push(fn(arr[i], i));
+    }
+    
+    return returnedArray;
+};
