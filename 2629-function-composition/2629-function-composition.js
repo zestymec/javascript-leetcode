649264ac0bfd3277ce2cd78973e366256aad7676
M Umer Aziz <m.umer394aziz@gmail.com>
2026-08-04 22:27:54 +0500

    Time: 50 ms (81.62%), Space: 55 MB (54.32%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        return functions.reduceRight((acc, fn) => fn(acc), x);
+    };
+};
